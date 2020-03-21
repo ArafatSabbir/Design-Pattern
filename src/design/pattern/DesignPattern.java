0fa729c5e54a9a774a46f2706787687cc54ae0a1
Sabbir <sabbirahmedcse33@gmail.com>
2020-03-22 02:37:38 +0600

    Update with intellij

--- src/design/pattern/DesignPattern.java
+++ src/design/pattern/DesignPattern.java
@@ -1,16 +1,11 @@
 package design.pattern;
 
-/**
- *
- * @author arafa
- */
+
 public class DesignPattern {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello");
     }
     
 }
