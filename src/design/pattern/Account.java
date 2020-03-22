58ae0f65cb4c4d4bdc7b77208dbf0107abd6e0ec
Sabbir <sabbirahmedcse33@gmail.com>
2020-03-22 12:31:10 +0600

    Encapsulation

--- src/design/pattern/Account.java
+++ src/design/pattern/Account.java
@@ -1,4 +1,19 @@
 package design.pattern;
 
 public class Account {
+    private float balance;
+
+    public void deposit(float amount){
+        if(amount>0)
+            balance+=amount;
+    }
+
+    public void withdraw(float amount){
+        if(amount>0)
+            balance-=amount;
+    }
+
+    public float getBalance(){
+        return balance;
+    }
 }
--- src/design/pattern/DesignPattern.java
+++ src/design/pattern/DesignPattern.java
@@ -5,13 +5,23 @@ public class DesignPattern {
 
 
     public static void main(String[] args) {
-        TaxCalculator calculator= getCalculator();
+        Account account=new Account();
+        account.deposit(10);
+        account.withdraw(5);
+        System.out.println(account.getBalance());
+
+
+
+        /*TaxCalculator calculator= getCalculator();
         calculator.texCalculator();
 
+         */
     }
 
-    public static TaxCalculator getCalculator(){
+   /* public static TaxCalculator getCalculator(){
         return new TaxCalculator2019();
     }
+
+    */
     
 }
